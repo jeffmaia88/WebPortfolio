8438dbe4676b2650d469aa7662da915ae7a13f03
Jefferson Maia <jefferson.reis88@gmail.com>
2025-07-25 18:09:45 -0300

    create cards

--- obj/Debug/net8.0/Site.AssemblyInfo.cs
+++ obj/Debug/net8.0/Site.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Site")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c81ae808f3bf450cf27f9f76742501dd51427a37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95d93624fe678c4c0747976e4ba2a6ee1655e480")]
 [assembly: System.Reflection.AssemblyProductAttribute("Site")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Site")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
